87ee370fac55b798533e219a40e0fc48745f4ac4
Vitaliy <modecom.pirogenko@gmail.com>
2024-06-03 11:49:08 +0300

    минификс

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import { useContext } from "react";
+import {useContext, useState} from "react";
 import AuthContext from "../context/AuthProvider";
 import Navigation from "./Navigation";
 import HomeCSS from "../styles/Home.module.css";
@@ -7,15 +7,25 @@ import HomeCSS from "../styles/Home.module.css";
 const Home = () => {
     const { setAuth } = useContext(AuthContext);
     const navigate = useNavigate();
+    const [token] = useState(localStorage.getItem('token'));
 
     const logout = async () => {
         localStorage.removeItem('token')
         setAuth({});
         navigate('/login');
     }
+    const loggedIn = token !== null;
 
     const products = async () => {
         navigate('/product');
+    }
+
+    const profile = async () => {
+        navigate('/user');
+    }
+
+    const cart = async () => {
+        navigate('/cart');
     }
 
     const login = async () => {
@@ -31,18 +41,34 @@ const Home = () => {
             <Navigation/>
             <section>
                 <h1>Добро пожаловать в магазин оптики "ПроЗрение"</h1>
-                <div className="flexGrow">
-                    <button onClick={register} className={HomeCSS.button}>Регистрация</button>
-                </div>
-                <div className="flexGrow">
-                    <button onClick={login} className={HomeCSS.button}>Войти</button>
-                </div>
+                {!loggedIn && (
+                    <div className="flexGrow">
+                        <button onClick={register} className={HomeCSS.button}>Регистрация</button>
+                    </div>
+                )}
+                {!loggedIn && (
+                    <div className="flexGrow">
+                        <button onClick={login} className={HomeCSS.button}>Войти</button>
+                    </div>
+                )}
                 <div className="flexGrow">
                     <button onClick={products} className={HomeCSS.button}>Список товаров</button>
                 </div>
-                <div className="flexGrow">
-                    <button onClick={logout} className={HomeCSS.button}>Выйти</button>
-                </div>
+                {loggedIn && (
+                    <div className="flexGrow">
+                        <button onClick={cart} className={HomeCSS.button}>Корзина</button>
+                    </div>
+                )}
+                {loggedIn && (
+                    <div className="flexGrow">
+                        <button onClick={profile} className={HomeCSS.button}>Профиль</button>
+                    </div>
+                )}
+                {!loggedIn && (
+                    <div className="flexGrow">
+                        <button onClick={logout} className={HomeCSS.button}>Выйти</button>
+                    </div>
+                )}
             </section>
         </div>
     )
